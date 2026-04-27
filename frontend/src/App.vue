e64a81f766195fbe0bda8ffd0a38aeb0374742b3
David Spilde <david.s.spilde@gmail.com>
2026-04-28 01:00:57 +0200

    add fish

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import SpinningFish from './components/SpinningFish.vue';
 </script>
 
 <template>
-  <HelloWorld />
+  <SpinningFish />
 </template>
---
+++ frontend/src/components/SpinningFish.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+      <img
+        class="fish"
+        src="https://media1.tenor.com/m/R5IECfIf34YAAAAd/fish-spinning.gif"
+        alt="spinning fish"
+      />
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background: #000;
+  }
+  
+  .fish {
+    width: 300px;
+    height: auto;
+  }
+  </style>
